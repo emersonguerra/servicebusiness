c1f1f5d29bc0f131792864773598397f4821e87c
emersonguerra <eguerra.com@gmail.com>
2025-06-03 00:57:07 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+import os
+
 class Config:
-    SECRET_KEY = 'OhblesqBom13$'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///finance.db'
+    # Secret Key usada para sessão, login etc.
+    SECRET_KEY = os.environ.get("SECRET_KEY", "OhblesqBom13$")
+
+    # Usa o banco do Render (PostgreSQL) se estiver definido, senão usa SQLite local
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///finance.db")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
