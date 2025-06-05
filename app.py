3acec300440893487ea924688dc2575f4d108857
emersonguerra <eguerra.com@gmail.com>
2025-06-05 05:36:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def create_app():
     
     # Blueprint Principal (dashboard, menu, etc.)
     from blueprints.main import main_bp
-    app.register_blueprint(main_bp)
+    app.register_blueprint(main_bp, url_prefix='/')
 
     # banco criacao
     @app.route('/initdb')
